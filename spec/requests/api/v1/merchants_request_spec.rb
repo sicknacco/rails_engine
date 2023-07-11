aaddb81cd6cb5392daa8557e5f1cd13c8959811b
Nick Sacco <nicksacco10@gmail.com>
2023-07-11 13:00:10 -0700

    feat: can find a merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,6 +10,7 @@ describe "Merchants API" do
       expect(response).to be_successful
       
       merchants = JSON.parse(response.body, symbolize_names: true)
+
       expect(merchants[:data].count).to eq(3)
       
       merchants[:data].each do |merchant|
@@ -25,19 +26,20 @@ describe "Merchants API" do
 
   describe "get api/v1/merchants/:id" do
     it "can get one merchant by it's id" do
-      id = create(:merchant).id
+      merchant1 = create(:merchant)
 
-      get "/api/v1/merchants/#{id}"
+      get "/api/v1/merchants/#{merchant1.id}"
 
       merchant = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
 
       expect(merchant[:data]).to have_key(:id)
-      expect(merchant[:data][:id]).to eq(id)
-      
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_an(String)
+      expect(merchant[:data][:id]).to be_a(String)
+      expect(merchant[:data][:id].to_i).to eq(merchant1.id)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_an(String)
     end
   end
 end
