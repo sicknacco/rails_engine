8bc3030c98da19949817b6d3085ee21095cffa40
Nick Sacco <nicksacco10@gmail.com>
2023-07-12 13:47:27 -0700

    feat: refactor update action to be a single line

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,9 +12,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    render json: ItemSerializer.new(item)
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
   end
 
   private
