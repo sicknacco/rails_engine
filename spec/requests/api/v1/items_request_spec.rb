aa549a602598b5b4387f457a42dd6ec197144bbb
Nick Sacco <nicksacco10@gmail.com>
2023-07-11 17:52:15 -0700

    test: display single item tests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,4 +30,28 @@ describe "Items API" do
       end
     end
   end
+  describe "get api/v1/items/:id" do
+    it "can get one item by it's id" do
+      item = create(:item)
+      
+      get "/api/v1/items/#{item.id}"
+      
+      item = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to be_successful
+      
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_a(String)
+      expect(item[:data][:id].to_i).to eq(item.id)
+      
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+    end
+  end
 end
