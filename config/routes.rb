8354e51f3f3ee51f65494a2b790aa1ed84f51d80
Nick Sacco <nicksacco10@gmail.com>
2023-07-12 15:51:56 -0700

    feat: merchant resources added to nested items with controller creation with action

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only:[:index], controller: "merchants/items"
+        resources :items, only: [:index], controller: "merchants/items"
       end
-      resources :items, only:[:index, :show, :create, :update, :destroy]
+      resources :items, except: [:new, :edit] do
+        resources :merchant, only: [:index], controller: "items/merchants"
+      end
     end
   end
 end
